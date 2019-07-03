610552aac6ea03132e89f39445634f4f4707cf14
Eugene Dronov <dronov-ep@yandex.ru>
2019-07-03 08:03:44 +0300

    start

---
+++ app/backend/create.sql
@@ -0,0 +1,15 @@
+PRAGMA key = 'Zk.,k.Lfie91';
+
+create table Users (
+  id integer primary key,
+  name text
+);
+
+insert into Users(name) values
+  ('admin'),
+  ('nerevarino'),
+  ('breynor')
+;
+
+
+
