321aeccbea74215b6dc774ce02181dd3018de426
Eugene Dronov <dronov-ep@yandex.ru>
2019-07-04 15:02:32 +0300

    04.07.2019 day

--- app/app.js
+++ app/app.js
@@ -1,5 +1,48 @@
-var carsApp = angular.module('carsApp', []);
+var carsApp = angular.module('carsApp', ['ngSQLite']);
 
 carsApp.controller('carsController', function($scope) {
     $scope.user_role = 0;
+
+    $scope.cars = [
+	{
+	    brand:'Kia',
+	    model:'Mohave',
+	    photo:'mohave'
+	},
+	{
+	    brand:'Audi',
+	    model:'Q7',
+	    photo:'q7'
+	},
+	{
+	    brand:'BMW',
+	    model:'X5',
+	    photo:'x5'
+	},
+	{
+	    brand:'Chevrolet',
+	    model:'Tahoe',
+	    photo:'tahoe'
+	},
+	{
+	    brand:'Mitsubishi',
+	    model:'Outlander',
+	    photo:'outlander'
+	},
+	{
+	    brand:'Toyota',
+	    model:'Land Cruiser 200',
+	    photo:'cruiser200'
+	},
+	{
+	    brand:'Renault',
+	    model:'Duster',
+	    photo:'duster'
+	},
+	{
+	    brand:'Nissan',
+	    model:'Pathfinder',
+	    photo:'pathfinder'
+	}	
+    ];
 });
