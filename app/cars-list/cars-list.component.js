ecb09a62b9f866be6d801460c9c82d4f8a80fa0e
Eugene Dronov <dronov-ep@yandex.ru>
2019-07-06 08:58:01 +0300

    final

--- app/cars-list/cars-list.component.js
+++ app/cars-list/cars-list.component.js
@@ -5,21 +5,46 @@ angular.module('carsList').component('carsList', {
     
     controller: function CarsListController($http) {
 
-	var self = this;
+	var self = this;  //для доступа к контроллеру из внутренних замыканий
 
 	const visitorRoleId = 0;
 	const adminRoleId = 1;
 
+	this.newCarLocation = '';   //Здесь содержится ссылка на новую позицию машины в Яндекс Картах   
 
-	this.newLocation = '';
-	this.changeLocation = function(carId) {
-	    self.db.cars[carId].location = self.newLocation;
-	    sessionStorage.mdb = JSON.stringify(self.db);
-	    self.newLocation = '';
+
+	
+	this.generateDBKey = function() {
+	    self.symKey = sjcl.random.randomWords(8, 8);
+	    sessionStorage.symKey = JSON.stringify(self.symKey);
+	};
+	this.restoreDBKey = function() {
+	    self.symKey = JSON.parse(sessionStorage.symKey);
+	};
+
+	
+	this.initDB = function(data) {
+	    sessionStorage.mdb = sjcl.encrypt(self.symKey, JSON.stringify(data));
+	    sessionStorage.used = true;		
+	    self.db = JSON.parse(sjcl.decrypt(self.symKey, sessionStorage.mdb));	    
+	};
+	this.restoreDB = function() {
+	    self.db = JSON.parse(sjcl.decrypt(self.symKey,sessionStorage.mdb));
 	};
 	
-	if(sessionStorage.used) {
-	    self.db = JSON.parse(sessionStorage.mdb);
+	
+	this.initVisitorParams = function() {
+	    self.roleId = visitorRoleId;
+	    self.roleName = self.db.roles[self.roleId].name;
+	    sessionStorage.roleId = self.roleId;
+	    sessionStorage.roleName = self.roleName;
+
+	    //ниже переменные для отключения ввода нового местоположения машины в случае не администраторской роли
+	    //изначально выдается роль посетителя до залогинивания под учетку администратора
+	    sessionStorage.locationFormsCount = 0;   
+	    self.locationForms = [];	    
+	};
+	this.restoreRoleParams = function() {   
 	    self.roleId = sessionStorage.roleId;
 	    self.roleName = sessionStorage.roleName;
 	    if(sessionStorage.locationFormsCount > 0) {
@@ -27,18 +52,30 @@ angular.module('carsList').component('carsList', {
 	    } else {
 		self.locationForms = [];				
 	    }
-	} else {
-	    $http.get('database/cars.db').then(function(response) {	   
-		sessionStorage.mdb = JSON.stringify(response.data);
-		sessionStorage.used = true;		
-		self.db = JSON.parse(sessionStorage.mdb);
-		self.roleId = visitorRoleId;
-		self.roleName = self.db.roles[self.roleId].name;
-		sessionStorage.locationFormsCount = 0;
-		self.locationForms = [];
-		sessionStorage.roleId = self.roleId;
-		sessionStorage.roleName = self.roleName;				
-	    });	    
-	}
+	};
+	
+	
+	this.onCreateComponent = function() {   //вызывается всегда при создании/пересоздании этого контроллера компонента
+	    if(sessionStorage.used) {     //если пересоздается      
+		this.restoreDBKey();
+		this.restoreDB();
+		this.restoreRoleParams();
+	    } else {         //если создается 
+		$http.get('database/cars.db').then(function(response) {		
+		    self.generateDBKey();
+		    self.initDB(response.data);
+		    self.initVisitorParams();
+		});	    
+	    }	    
+	};
+
+	this.changeLocation = function(carId) {    //функция - внешний интерфейс контроллера - обработчик ввода новой позиции машины
+	    self.db.cars[carId].location = self.newCarLocation;
+	    sessionStorage.mdb = sjcl.encrypt(self.symKey, JSON.stringify(self.db));	    
+	    self.newCarLocation = '';
+	};
+	
+
+	this.onCreateComponent();
     }
 });
--- app/log-in/log-in.component.js
+++ app/log-in/log-in.component.js
@@ -5,17 +5,32 @@ angular.module('logIn').component('logIn',  {
     
     controller: function LogInController($http) {
 
-	var self = this;
+	var self = this;  //для доступа к контроллеру из внутренних замыканий
 
-	this.username = '';
-	this.password = '';
-	this.message = '';
+	this.username = '';      //модель введенного посетителем имя пользователя
+	this.password = '';      //модель введенного посетителем пароля пользователя 
+	this.message = '';       //модель сообщения посетителю в зависимости от результата проверки логина и пароля
 
-	this.db = JSON.parse(sessionStorage.mdb);
-	this.roleId = sessionStorage.roleId;
-	this.roleName = sessionStorage.roleName;
+	this.restoreDBKey = function() {
+	    self.symKey = JSON.parse(sessionStorage.symKey);
+	};	
+	this.getDB = function() {
+	    self.db = JSON.parse(sjcl.decrypt(self.symKey,sessionStorage.mdb));	    
+	};
 
-	this.tryLogin = function() {
+	this.getRoleParams = function() {
+	    this.roleId = sessionStorage.roleId;
+	    this.roleName = sessionStorage.roleName;	    
+	}
+
+	this.onCreateComponent = function() {    //вызывается всегда при создании/пересоздании этого контроллера компонента
+	    self.restoreDBKey();
+	    self.getDB();
+	    self.getRoleParams();
+	};
+	
+
+	this.tryLogin = function() {     //функция - внешний интерфейс контроллера - обработчик попытки входа под пользователем с паролем
 	    for(let user of self.db.users) {
 		if(self.username == user.name) {
 		    if(self.password == user.passwd) {
@@ -33,5 +48,7 @@ angular.module('logIn').component('logIn',  {
 		}
 	    }
 	};
+
+	this.onCreateComponent();
     }
 });
